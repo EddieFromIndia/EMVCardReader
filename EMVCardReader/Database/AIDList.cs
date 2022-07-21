5feb372ce2e8ceb8a19c151748dfbe2e98ba83d3
EddieFromIndia <bassistatanu@gmail.com>
2022-07-21 21:16:31 +0530

    Reverted AID list change

--- EMVCardReader/Database/AIDList.cs
+++ EMVCardReader/Database/AIDList.cs
@@ -4,107 +4,70 @@ namespace EMVCardReader.Database
 {
     public static class AIDList
     {
-        public static List<string> List = new List<string>()
-        {
-            "A0000000011010",        // Danmønt Cash
-            "A000000001",            // Danmønt
-            "A000000003101001",      // Visa Credit
-            "A000000003101002",      // Visa Debit
-            "A0000000032010",        // Visa electron
-            "A0000000032020",        // V Pay
-            "A0000000033010",        // Visa Interlink
-            "A0000000034010",        // Visa
-            "A0000000035010",        // Visa
-            "A0000000036010",        // Domestic Visa Cash Stored Value
-            "A0000000036020",        // International Visa Cash Stored Value
-            "A0000000038002",        // Barclays Or HBOS
-            "A0000000038010",        // Visa Plus
-            "A0000000039010",        // Visa Loyalty
-            "A000000003999910",      // Visa ATM
-            "A000000003",            // Visa
-            "A000000098",            // Bank Of America Debit Visa
-            "A00000000410101213",    // Mastercard
-            "A00000000410101215",    // Mastercard
-            "A000000004110101213",   // Mastercard Credit
-            "A0000000042010",        // Mastercard
-            "A0000000042203",        // Mastercard
-            "A0000000043010",        // Mastercard
-            "A0000000043060",        // Maestro
-            "A0000000044010",        // Mastercard
-            "A0000000045010",        // Mastercard
-            "A0000000046000",        // Cirrus
-            "A0000000048002",        // NatWest or SecureCode Auth
-            "A0000000049999",        // Mastercard
-            "A000000004",            // Mastercard
-            "A0000000050001",        // Switch Debit Maestro
-            "A0000000050002",        // Switch Debit Solo
-            "A000000005",            // Switch
-            "A0000000250000",        // Amex
-            "A000000025010402",      // Amex
-            "A000000025010701",      // Amex ExpressPay
-            "A000000025010801",      // Amex
-            "A00000002501",          // Amex
-            "A000000025",            // Amex
-            "A00000079001",          // Amex China
-            "A000000790",            // Amex China
-            "A000000817002001",      // Menards
-            "A0000000291010",        // LINK ATM
-            "A0000000421010",        // French CB
-            "A0000000422010",        // French CB Debit
-            "A000000042",            // French CB
-            "A0000000651010",        // JCB Smart
-            "A000000065",            // JCB
-            "A0000001211010",        // Dankort
-            "A0000001214711",        // Dankort Visa
-            "A0000001214712",        // Dankort JSpeedy
-            "A000000121",            // Dankort
-            "A0000001410001",        // Pagobancomat Debit
-            "A000000141",            // Pagobancomat
-            "A00000035910100380",    // Pagobancomat
-            "A0000001523010",        // Diners Club Discover
-            "A000000152",            // Discover
-            "A0000003241010",        // Discover
-            "A00000006900",          // FR Moneo
-            "A0000000980840",        // Visa Common Debit
-            "A0000000980848",        // Schwab Bank Debit
-            "A0000001544442",        // Banricompras Debito Brazil
-            "A0000001850002",        // UK Post Office Card Account card
-            "A0000002281010",        // SPAN
-            "A0000002282010",        // SPAN
-            "A0000002771010",        // Interac
-            "A000000333010101",      // UnionPay Debit
-            "A000000333010102",      // UnionPay Credit
-            "A000000333010103",      // UnionPay QuasiCredit
-            "A000000333010106",      // UnionPay Electronic Cash
-            "A000000333",            // UnionPay
-            "A0000003156020",        // Chipknip
-            "A0000003591010028001",  // ZKA Girocard
-            "A0000003710001",        // InterSwitch Verve
-            "A0000004391010",        // Exchange Network ATM
-            "A0000006300101",        // Dinube
-            "A0000006581010",        // MIR Credit
-            "A0000006582010",        // MIR Debit
-            "A000000658",            // MIR
-            "A0000004360100",        // Edenred
-            "A00000038410",          // EFTPOS Savings
-            "A00000038420",          // EFTPOS Cheque
-            "A000000384",            // EFTPOS
-            "A000000337301000",      // GIM UEMOA Retrait
-            "A000000337101000",      // GIM UEMOA Standard
-            "A000000337102000",      // GIM UEMOA Classic
-            "A000000337101001",      // GIM UEMOA Prepay Online
-            "A000000337102001",      // GIM UEMOA Prepay Offline
-            "A000000337601001",      // GIM UEMOA Porte Monnaie Electronique
-            "A000000337",            // GIM UEMOA Classic
-            "A000000732100123",      // Meeza
-            "A0000005291010",        // Mercury
-            "A0000004540010",        // Etranzact Genesis
-            "A0000004540011",        // Etranzact Genesis
-            "A0000004766C",          // Google
-            "A0000005241010",        // RuPay
-            "A000000620",            // US Debit DNA
-            "D27600002545500100",    // ZKA Girocard
-            "D5780000021010",        // BankAxept
+        public static List<string> List = new List<string>(){
+          "A0000000031010",        // Visa credit or debit
+          "A000000003101001",      // VISA Credit
+          "A000000003101002",      // VISA Debit
+          "A0000000032010",        // Visa electron
+          "A0000000032020",        // V pay
+          "A0000000033010",        // VISA Interlink
+          "A0000000034010",        // VISA Specific
+          "A0000000035010",        // VISA Specific
+          "A0000000036010",        // Domestic Visa Cash Stored Value
+          "A0000000036020",        // International Visa Cash Stored Value
+          "A0000000038002",        // Barclays/HBOS
+          "A0000000038010",        // Visa Plus
+          "A0000000039010",        // VISA Loyalty
+          "A000000003999910",      // VISA ATM
+          "A000000004",            // US Debit (MC)
+          "A0000000041010",        // Mastercard credit or debit
+          "A00000000410101213",    // MasterCard
+          "A00000000410101215",    // MasterCard
+          "A000000004110101213",   // Mastercard Credit
+          "A0000000042010",        // MasterCard Specific
+          "A0000000043010",        // MasterCard Specific
+          "A0000000043060",        // Mastercard Maestro
+          "A0000000044010",        // MasterCard Specific
+          "A0000000045010",        // MasterCard Specific
+          "A0000000046000",        // Mastercard Cirrus
+          "A0000000048002",        // NatWest or SecureCode Auth
+          "A0000000049999",        // Mastercard
+          "A0000000050001",        // UK Domestic Maestro - Switch (debit card) Maestro
+          "A0000000050002",        // UK Domestic Maestro - Switch (debit card) Solo
+          "A0000000250000",        // American Express (Credit/Debit)
+          "A00000002501",          // American Express
+          "A000000025010402",      // American Express
+          "A000000025010701",      // American Express ExpressPay
+          "A000000025010801",      // American Express
+          "A0000000291010",        // ATM card LINK (UK) ATM network
+          "A0000000421010",        // French CB
+          "A0000000422010",        // French CB
+          "A00000006510",          // JCB
+          "A0000000651010",        // JCB
+          "A00000006900",          // FR Moneo
+          "A000000098",            // Bank of America Debit Card (Visa)
+          "A0000000980840",        // Visa Common Debit Card
+          "A0000000980848",        // Schwab Bank Debit Card
+          "A0000001211010",        // Dankort Danish domestic debit card
+          "A0000001410001",        // Pagobancomat Italian domestic debit card
+          "A000000152",            // US Debit (Discover)
+          "A0000001523010",        // Diners club/Discover
+          "A0000001544442",        // Banricompras Debito (Brazil)
+          "A0000001850002",        // UK Post Office Card Account card
+          "A0000002281010",        // SAMA Saudi Arabia domestic credit/debit card
+          "A0000002282010",        // SAMA Saudi Arabia domestic credit/debit card
+          "A0000002771010",        // Interac
+          "A0000003156020",        // Chipknip
+          "A0000003241010",        // Discover
+          "A0000003591010028001",  // ZKA Girocard (Germany)
+          "A0000003710001",        // InterSwitch Verve Card (Nigeria)
+          "A0000004540010",        // Etranzact Genesis Card (Nigeria)
+          "A0000004540011",        // Etranzact Genesis Card 2 (Nigeria)
+          "A0000004766C",          // Google
+          "A0000005241010",        // RuPay (India)
+          "A000000620",            // US Debit (DNA)
+          "D27600002545500100",    // ZKA Girocard (Germany)
+          "D5780000021010",        // BankAxept Norwegian domestic debit card
         };
     }
 }
